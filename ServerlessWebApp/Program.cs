06248d2bf325113ca632acef49b37110ebeed46a
anantakhanal16 <anantaramankhanal@gmail.com>
2024-08-21 22:15:10 +0545

    initial push

---
+++ ServerlessWebApp/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
+// package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+
+    // Set Swagger UI at the app's root. This makes Swagger UI available at the base URL (e.g., http://your-lambda-url).
+    c.RoutePrefix = string.Empty;
+
+});
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapGet("/", () => "Welcome to running ASP.NET Core Minimal API on AWS Lambda");
+
+app.Run();
